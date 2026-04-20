2867e69cad1502162cdf6dfb2784914a16467e94
Fernandobarrenechea09 <fbarrenechea2023@scis-bo.com>
2026-04-19 23:01:02 -0400

    dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -15,8 +15,7 @@ import pandas as pd
 # ─────────────────────────────────────────────
 
 st.set_page_config(
-page_title=“page_title="LEADBOOST // DASHBOARD",
-
+page_title=“LEADBOOST // DASHBOARD”,
 page_icon=“■”,
 layout=“wide”
 )
