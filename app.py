07a730de5453c7100d82969a37203293e0a542ff
Fernandobarrenechea09 <fbarrenechea2023@scis-bo.com>
2026-04-20 10:23:51 -0400

    app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from email.mime.multipart import MIMEMultipart
 # ======================================================
 
 st.set_page_config(
-page_title=“LEADBOOST”,
+page_title="LEADBOOST",
 page_icon=”#”,
 layout=“wide”,
 initial_sidebar_state=“collapsed”
